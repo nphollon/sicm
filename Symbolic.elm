7661dd22aec3739b34ffdd935f40b12659b6a433
Nick Hollon <falling.masonry@gmail.com>
2015-05-10 17:37:00 -0700

    OMG symbolic parsing

--- Symbolic.elm
+++ Symbolic.elm
@@ -3,46 +3,65 @@ module Symbolic where
 import Dict exposing (Dict)
 import String
 
-type alias Expression = List Symbol
-type Symbol = Unknown | Number Float | Plus
-type Error = StackEmpty | StackTooBig | BadToken String
+type Expression =
+  Symbol String |
+  Number Float |
+  Binary (Float -> Float -> Float) Expression Expression |
+  Unary (Float -> Float) Expression
+
+type Error = StackEmpty | StackTooBig
 
 (...) = Result.andThen
 
-evaluate v x f = lex x f ... valueAt v
+--evaluate : Float -> String -> String -> Float
+--evaluate v x f = lex f ... parse ... substitute x v ... reduce
 
-lex : String -> String -> Result Error Expression
-lex unknown text = 
-  let
-    symbolTable = Dict.fromList [("+", Plus), ("1", Number 1), (unknown, Unknown)]
-    mergeResult token lexedSymbols = Result.map2 (::) (identify token symbolTable) lexedSymbols 
-  in List.foldr mergeResult (Ok []) (String.words text)
+lex : String -> Result Error (List String)
+lex text = Ok (String.words text)
 
-identify : String -> Dict String Symbol -> Result Error Symbol
-identify token symbols =
-  case String.toFloat token of
-    Ok f -> Ok (Number f)
-    Err _ -> Dict.get token symbols |> Result.fromMaybe (BadToken token)
-
-valueAt : Float -> Expression -> Result Error Float
-valueAt x f =
+parse : List String -> Result Error Expression
+parse tokens =
   let
     stackResult = Ok []
-    parseNext symbol result = result ... (parseWith x symbol)
-  in List.foldl parseNext stackResult f ... pullFromStack
+    parseNext token result = result ... (parseWith token)
+  in List.foldl parseNext stackResult tokens ... pullFromStack
 
-parseWith : Float -> Symbol -> List Float -> Result Error (List Float)
-parseWith x c stack = case c of
-  Unknown -> Ok <| x :: stack
-  Number c -> Ok <| c :: stack
-  Plus -> apply (+) stack
+parseWith : String -> List Expression -> Result Error (List Expression)
+parseWith token stack =
+  case Dict.get token symbolTable of
+    Just op -> op stack
+    Nothing -> case String.toFloat token of
+      Ok x -> push (Number x) stack
+      Err _ -> push (Symbol token) stack
 
-apply : (Float -> Float -> Float) -> List Float -> Result Error (List Float)
-apply op stack = case stack of
-  x :: y :: rest -> Ok <| (op x y) :: rest
+symbolTable : Dict String (List Expression -> Result Error (List Expression))
+symbolTable = Dict.fromList [
+  ("pi", push (Number pi)),
+  ("+", binaryOp (+)),
+  ("-", binaryOp (+)),
+  ("*", binaryOp (+)),
+  ("/", binaryOp (+)),
+  ("^", binaryOp (+)),
+  ("~", unaryOp negate),
+  ("exp", unaryOp ((^) e)),
+  ("log", unaryOp (logBase e)),
+  ("sin", unaryOp sin),
+  ("cos", unaryOp cos) ]
+
+binaryOp : (Float -> Float -> Float) -> List Expression -> Result Error (List Expression)
+binaryOp op stack = case stack of
+  (y :: x :: rest) -> push (Binary op x y) rest
   otherwise -> Err StackEmpty
 
-pullFromStack : List Float -> Result Error Float
+unaryOp : (Float -> Float) -> List Expression -> Result Error (List Expression)
+unaryOp op stack = case stack of
+  (x :: rest) -> push (Unary op x) rest
+  otherwise -> Err StackEmpty
+
+push : a -> List a -> Result Error (List a)
+push x = Ok << (::) x
+
+pullFromStack : List a -> Result Error a
 pullFromStack s = case s of
   [ y ] -> Ok y
   [] -> Err StackEmpty
